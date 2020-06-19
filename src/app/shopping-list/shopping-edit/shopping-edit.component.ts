06bfed4caad97fa60ba5f2fa334f05aeaefecc40
Alen Thankachan <alenthankz@gmail.com>
2020-06-19 05:08:08 -0700

    Till shopping list validation just for reference

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -10,24 +10,23 @@ import {
 import { Ingredient } from '../../shared/ingredient.model';
 import {ShoppingListService} from'../shopping-list.service';
 import { from } from 'rxjs';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+ 
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form:NgForm) {
+  const value=form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 
