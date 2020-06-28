b5af17a0984e3a7e6cb68c8ea5b2af5ff759a11f
Alen Thankachan <alenthankz@gmail.com>
2020-06-28 03:24:07 -0700

    deployed firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { DataStorageService } from './shared/data-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService:AuthService){}
+  constructor(private authService:AuthService,private dataStorageService:DataStorageService){}
   ngOnInit(){
     this.authService.autoLogin();
   }
